7e6ebfa7fd5889d3674e61ba472cc9fe1bf59693
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-30 17:27:33 +0100

    Update initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,8 @@
-// test(("First basic test"), () => {
-//     expect("APP").toBe
-// })
-export {};
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("render HELLO on app component", () => {
+    render(<App />);
+    const textElement = screen.getByText("HELLO");
+    expect(textElement).toBeInTheDocument();
+});
